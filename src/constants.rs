79ba4f24e9dd9a42f7da44a1ba46f89f4d933d80
Ole Andre Birkedal <birkedal@extab.net>
2025-05-03 21:40:08 +0200

    added secret constants

---
+++ src/constants.rs
@@ -0,0 +1,8 @@
+const CLIENT_ID: &str = "secret";
+const CLIENT_TYPE: &str = "android_8.62.2";
+const DEVICE_UID: &str = "secret";
+const FIREBASE_UID: &str = "secret";
+const FIREBASE_JWT: &str = "secret";
+const API_SERVER: &str = "https://api.jodelapis.com/api/";
+const API_PATH_V2: &str = "v2/";
+const API_PATH_V3: &str = "v3/";
