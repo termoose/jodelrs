6ab659215bf0df52954b31f6ee5fc38db1515040
Ole Andre Birkedal <birkedal@extab.net>
2025-05-03 21:38:46 +0200

    started on request module

--- src/request.rs
+++ src/request.rs
@@ -1,3 +1,40 @@
+use serde::{Deserialize, Serialize};
+
+pub struct Request {
+    token: Option<String>,
+}
+
+#[derive(Debug, Serialize)]
+struct Coordinates {
+    lat: f32,
+    lng: f32,
+}
+
+#[derive(Debug, Serialize)]
+struct Location {
+    city: String,
+    country: String,
+    loc_accuracy: String,
+    loc_coordinates: Coordinates,
+}
+
+#[derive(Debug, Serialize)]
+struct AccessTokenBody {
+    language: String,
+    client_id: String,
+    device_uid: String,
+    firebase_uid: String,
+    #[serde(rename = "firebaseJWT")]
+    firebase_jwt: String,
+    location: Location,
+}
+
+impl Request {
+    pub fn new() -> Self {
+        Request { token: None }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
